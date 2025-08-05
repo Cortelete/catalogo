0a3f819dbe6972d386adac6eb2539be09f488c6a
Cortelete <d.cortelete@gmail.com>
2025-08-05 15:51:34 -0300

    teste

---
+++ src/data/procedures.ts
@@ -0,0 +1,41 @@
+import { Category } from '../types';
+
+import { lashSubCategories } from './lash/procedures';
+import { eyebrowSubCategories } from './designSobrancelhas/procedures';
+import { skinSubCategories } from './pele/procedures';
+import { advancedSubCategories } from './esteticaAvancada/procedures';
+import { nanoProcedures } from './nanopigmentacao/procedures';
+import { hairProcedures } from './cabelos/procedures';
+
+export const CATEGORIES: Category[] = [
+  {
+    id: 'lash',
+    name: 'Lash - Cílios',
+    subCategories: lashSubCategories,
+  },
+  {
+    id: 'designSobrancelhas',
+    name: 'Design de Sobrancelhas',
+    subCategories: eyebrowSubCategories,
+  },
+   {
+    id: 'cabelos',
+    name: 'Cabelos',
+    procedures: hairProcedures,
+  },
+  {
+    id: 'limpezaPele',
+    name: 'Cuidados com a Pele',
+    subCategories: skinSubCategories,
+  },
+  {
+    id: 'botoxDay',
+    name: 'Estética Avançada',
+    subCategories: advancedSubCategories,
+  },
+  {
+    id: 'nanopigmentacao',
+    name: 'Nanopigmentação',
+    procedures: nanoProcedures,
+  },
+];
