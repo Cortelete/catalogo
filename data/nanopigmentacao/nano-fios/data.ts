599dff50fd80652feeead611a101084f07ee70cd
Cortelete <d.cortelete@gmail.com>
2025-08-05 15:39:44 -0300

    catalogolux

---
+++ data/nanopigmentacao/nano-fios/data.ts
@@ -0,0 +1,15 @@
+import { Procedure } from '../../../types';
+
+export const nanoFios: Procedure = {
+  id: 'nano-fios',
+  name: 'Nanopigmentação de Sobrancelhas',
+  description: 'Técnica avançada de micropigmentação que cria fios ultra-realistas, resultando em sobrancelhas perfeitas e naturais.',
+  price: 'Em breve',
+  comingSoon: true,
+  details: ['Tecnologia de ponta para precisão', 'Longa duração com retoques anuais'],
+  images: [
+      'data/nanopigmentacao/nano-fios/img/NF1.png',
+      'data/nanopigmentacao/nano-fios/img/NF2.png',
+      'data/nanopigmentacao/nano-fios/img/NF3.png',
+  ],
+};
